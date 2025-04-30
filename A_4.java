adac53678fc9e8dddbb7b96f2965715741c84a1e
unknown <kathanmajithia@gmail.com>
2025-04-30 16:35:18 +0530

    Added practice 4

---
+++ A_4.java
@@ -0,0 +1,10 @@
+class A_3{
+	public static void main(String... args){
+		int data[] = {1,3,5,6,8};
+
+		System.out.print("\nReversed array : ");
+		for(int i=data.length-1;i>=0;i--)
+			System.out.print(data[i] + " ");
+
+}
+}
