dbfa0e6464be9aabee99d3555f0a95578411fde3
unknown <kathanmajithia@gmail.com>
2025-05-01 13:35:08 +0530

    Wrap

---
+++ Wrap.java
@@ -0,0 +1,12 @@
+class Wrap{
+        public static void main(String... args){
+        Integer on = 200;
+        Character co = 'K';
+        Boolean bo = true;
+
+        System.out.println("Integer value : " + on);
+        System.out.println("Char value : " + co);
+        System.out.println("Boolean value : " + bo);    
+
+    }
+}
