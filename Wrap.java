67576a1aad7b19d52bdecb0dc48d72c3d2d036ad
unknown <kathanmajithia@gmail.com>
2025-05-01 13:35:18 +0530

    Wrap modified

--- Wrap.java
+++ Wrap.java
@@ -8,5 +8,11 @@ class Wrap{
         System.out.println("Char value : " + co);
         System.out.println("Boolean value : " + bo);    
 
+        System.out.println("Binary : " + Integer.toBinaryString(120));
+        System.out.println("MAX : " + Integer.MAX_VALUE);
+        System.out.println("MIN : " + Integer.MIN_VALUE);
+        System.out.println("REVERSE : " + Integer.reverse(345));
+        System.out.println("SIZE : " + Integer.SIZE);
+        System.out.println("HEX : " + Integer.toHexString(120));
     }
 }
