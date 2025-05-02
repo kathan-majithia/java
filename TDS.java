4d72acafaf3f33720db2a120724239b4034d18cc
unknown <kathanmajithia@gmail.com>
2025-05-02 17:24:36 +0530

    Added TDS

---
+++ TDS.java
@@ -0,0 +1,22 @@
+class TDS{
+    String name;
+    double basic_sal,dearness_allowance;
+    double gross_sal,tax;
+    TDS(String n,double sal,double allow){
+        name = n;
+        basic_sal = sal;
+        dearness_allowance = allow;
+        gross_sal = sal + allow;
+    }
+    public static void main(String... args) {
+        TDS per1 = new TDS("Nisarg",95000,30000);
+        TDS per2 = new TDS("Harsh",45000,12000);
+
+        per1.tax_deducted();
+        per2.tax_deducted();
+
+        per1.display();
+        System.out.println("\n");
+        per2.display();
+    }
+}
