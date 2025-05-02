f1050f6c395d9b0c09082d37784a2d3787eb4c73
unknown <kathanmajithia@gmail.com>
2025-05-02 17:24:54 +0530

    Tax dedected

--- TDS.java
+++ TDS.java
@@ -8,6 +8,11 @@ class TDS{
         dearness_allowance = allow;
         gross_sal = sal + allow;
     }
+    void tax_deducted(){
+        tax = 0;
+        if (gross_sal > 100000)
+            tax = (gross_sal - 100000) * 0.1;
+    }
     public static void main(String... args) {
         TDS per1 = new TDS("Nisarg",95000,30000);
         TDS per2 = new TDS("Harsh",45000,12000);
