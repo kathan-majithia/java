2ece331693fd19f3cfa043901c407636bf2985bf
unknown <kathanmajithia@gmail.com>
2025-05-03 08:48:25 +0530

    Added Flat

--- Inheritance/P_4_3.java
+++ Inheritance/P_4_3.java
@@ -1,6 +1,30 @@
  interface Property{
     public void computePrice();
  }
+ class Bungalow implements Property{
+    String name;
+    double constructionArea;
+    double landArea;
+
+    Bungalow(String n,double ca,double la){
+        name = n;
+        constructionArea = ca;
+        landArea = la;
+    }
+    public void computePrice(){
+        double tot = 0;
+        tot += (500 * constructionArea);
+        System.out.println("Construction cost : Rs." + tot);
+        System.out.println("Additional cost : Rs." + (200 * landArea));
+        tot += 200 * landArea;
+        System.out.println("Land Cost : Rs." + (400 * landArea));
+        tot += 400 * landArea;
+
+        System.out.println("Total cost for bungalow of "+name+
+        " having\nConstruction Area = " + constructionArea + 
+        "sq feet\nLand area : " + landArea+ "sq feet is Rs. " + tot);
+    }
+ }
  class P_4_3{
 
     public static void main(String... args) {
