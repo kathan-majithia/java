5cd0f733f9875e47d9691e554eb76be4cf6ca991
Pushpraj <parthchavda695@gmail.com>
2025-05-04 21:42:37 +0530

    Added method local inner class

--- P_5_2.java
+++ P_5_2.java
@@ -1,7 +1,22 @@
 
+class Meth_outer{
+    void call(){
+        System.out.println("Calling from method local outer class.");
+        class Meth_Inner{
+            void in_call(){
+                System.out.println("Calling from Method local inner class.");
+            }
+        }
+        Meth_Inner mi = new Meth_Inner();
+        mi.in_call();
+    }
+}
+
+
 public class P_5_2{
     public static void main(String... args) {
-
+        Meth_outer mo = new Meth_outer();
+        mo.call();
 
         Ano_Inner aio = new Ano_Inner(){
             public void call(){
