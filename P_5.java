61505c9b1a2ab01b35ce88ac8ca2ef9829b40ae1
Pushpraj <parthchavda695@gmail.com>
2025-05-04 21:40:45 +0530

    Added processor inner class

--- P_5.java
+++ P_5.java
@@ -5,6 +5,23 @@ class CPU {
     CPU(double p) {
         price = p;
     }
+    class Processor {
+        double cores, cache;
+        String manufacturer;
+
+        Processor(double c, double ca, String m) {
+            cores = c;
+            cache = ca;
+            manufacturer = m;
+        }
+        double getCache() {
+            return cache;
+        }
+        void displayProcessorDetail() {
+            System.out.println("\nProcessor details...");
+            System.out.println("Cores : " + cores + " : Manufacturer : " + manufacturer);
+        }
+    }
 }
 public class P_5 {
     public static void main(String... args) {
