1204d37b651cf5358e9e19d2910b1c3607112851
unknown <kathanmajithia@gmail.com>
2025-05-07 21:09:00 +0530

    Added withdraw method

--- P_7_1.java
+++ P_7_1.java
@@ -13,6 +13,15 @@ class Bank{
         bal += d;
         System.out.println("New Balance : " + bal);
     }
+    void withdraw(int w) throws ArithmeticException,NotEnoughMoneyException{
+        if (w > bal)
+            throw new ArithmeticException("Withdraw money is more than balance.");
+        if (w - bal < 500)
+            throw new NotEnoughMoneyException("Minimum balance should be greater than 500");  
+        System.out.println("Old balance : " + bal);
+        bal -= w;
+        System.out.println("New Balance : " + bal);     
+    }
 }
 public class P_7_1{
     public static void main(String... args){
