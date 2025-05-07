9f54f10e9e61145b597770fa10af3e224fb22387
unknown <kathanmajithia@gmail.com>
2025-05-07 21:08:02 +0530

    Not enough money exception

---
+++ P_7_1.java
@@ -0,0 +1,20 @@
+class NotEnoughMoneyException extends Exception{
+    public NotEnoughMoneyException(String m){
+        super(m);
+    }
+}
+public class P_7_1{
+    public static void main(String... args){
+        Bank b = new Bank(10000);
+
+        b.deposit(4000);
+        try{
+            b.withdraw(13600);
+        }
+        catch(Exception e){
+            System.out.println("Caught");
+            System.out.println(e.getMessage());
+        }
+    }
+}
+
