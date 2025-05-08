a644aaeb580704da4b16714442bfde2f02a4e62f
unknown <kathanmajithia@gmail.com>
2025-05-08 17:42:59 +0530

    0 exception

--- P_7_2.java
+++ P_7_2.java
@@ -7,7 +7,19 @@ public class P_7_2{
         int n = sc.nextInt();
 
         int sum = 0;
+        try{
+        if (n == 0)
+            throw new ArithmeticException("Number of integers is zero.");
+        System.out.println("Enter numbers : ");
         
+        }
+        catch(ArithmeticException e){
+            System.out.println("Caught");
+            System.out.println(e.getMessage());
+        }catch(NumberFormatException e){
+            System.out.println("Caught");
+            System.out.println(e.getMessage());
+        }
     }
 }
 
