fdc5ffaeb807e63f57ba77198e63c2c925105c4f
unknown <kathanmajithia@gmail.com>
2025-05-08 17:41:49 +0530

    Added Custom exception

---
+++ P_7_2.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class P_7_2{
+    public static void main(String... args) throws ArithmeticException3{
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number of integers to be entered : ");
+        int n = sc.nextInt();
+
+        int sum = 0;
+        
+    }
+}
+
