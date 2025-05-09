bc2f28856109b51dea591088d8584a28b71af151
unknown <kathanmajithia@gmail.com>
2025-05-09 16:31:16 +0530

    Thread1 using thread

--- P_8_1.java
+++ P_8_1.java
@@ -1,6 +1,34 @@
 
 import java.io.*;
 
+class Thread1 extends Thread{
+    int s,e;
+    Thread1(int s,int e){
+        this.s = s;
+        this.e = e;
+    }
+    public void run(){
+        try{
+            for(int i=s;i<=e;i++){
+                int fl = 0;
+                for(int j = 2;j < i;j++){
+                    if (i % 2 == 0){
+                        fl = 1;
+                        break;
+                    }
+                }
+                if (fl == 0)
+                    System.out.println("by thread : " + i);
+                    Thread.sleep(0);
+            }
+        }
+        catch(InterruptedException e){
+            System.out.println(e);
+        }
+    }
+}
+
+
 public class P_8_1{
     public static void main(String... args){
         int s = 2,e = 20;
