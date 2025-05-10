2082d06d55e1b3935e1b223ba33be9e40b4d8ce2
unknown <kathanmajithia@gmail.com>
2025-05-10 10:23:30 +0530

    Produce added

--- TestThread.java
+++ TestThread.java
@@ -11,11 +11,28 @@ class Queuee{
         notifyAll();
     }
 }
+class Producer implements Runnable{
+    Queuee q;
+    Producer(Queuee q){
+        this.q = q;
+    }
+    public void produce(int i) throws InterruptedException{
+        synchronized(q){
+
+        while(q.qu.size() == q.size)
+            q.wait();
+        q.qu.add(i);
+        System.out.println("Produced : " + i);
+        q.nop();
+        }
+    }
+}
 public class TestThread {
     public static void main(String... args) {
         Queuee q = new Queuee(15);  
         
 
+        
     }
 }
 
