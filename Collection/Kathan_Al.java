9c641b8cee2099abf24034eb1ab7f8afabf8e491
unknown <kathanmajithia@gmail.com>
2025-05-11 10:23:27 +0530

    Arraylist added

---
+++ Collection/Kathan_Al.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Kathan_Al{
+    public static void main(String... args) {
+        
+        ArrayList<String> al = new ArrayList<>();
+        al.add("Kathan");
+        al.add("Majithia");
+        al.add("Kamy");
+        al.add("Nisarg");
+        System.out.println("Size : " + al.size());
+        al.remove("Nisarg");
+        System.out.println("Content of Array List : " + al);
+
+    }
+}
+
+
+
+
+
