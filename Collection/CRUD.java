cca2f4ceb7d7a10e2053ea46a21591dea5057e64
unknown <kathanmajithia@gmail.com>
2025-05-12 12:45:56 +0530

    Added insert

--- Collection/CRUD.java
+++ Collection/CRUD.java
@@ -1,9 +1,18 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Kathan_sort {
     public static void main(String... args) {
         ArrayList<Integer> al = new ArrayList<>();
-       
+        Scanner sc = new Scanner(System.in);
+        while(true){
+            System.out.println("\nEnter : \n1 to insert,2 to update, 3 to delete,4 to exit");
+            int ch = sc.nextInt();
+            switch (ch) {
+                case 1:System.out.println("Enter value : ");
+                        al.add(sc.nextInt());
+                    break;
+        }
     }
 }
 
