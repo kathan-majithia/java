23ddddaaa27a8d69bfc4b7da9c2067301cc3ad5c
unknown <kathanmajithia@gmail.com>
2025-05-12 12:45:17 +0530

    Added crud

---
+++ Collection/CRUD.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class Kathan_sort {
+    public static void main(String... args) {
+        ArrayList<Integer> al = new ArrayList<>();
+       
+    }
+}
+
+
+
