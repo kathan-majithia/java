83d205284b3226c75eda91816763e08adca46774
unknown <kathanmajithia@gmail.com>
2025-05-13 16:00:19 +0530

    Added Arraylist sort

--- Collection/col_sort.java
+++ Collection/col_sort.java
@@ -9,5 +9,17 @@ public class col_sort {
             System.out.print(" " + a);
         Arrays.sort(ar);
 
+        ArrayList<Integer> al = new ArrayList<>();
+        al.add(5);
+        al.add(2);
+        al.add(1);
+        al.add(6);
+        al.add(8);
+
+        System.out.print("Before sorted : " + al);
+
+        al.sort(null);
+
+        System.out.print("After sorted : " + al);
     }
 }
