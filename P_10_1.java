3cbda168744450a0e6ec06cc42a62c52b75a0c42
unknown <kathanmajithia@gmail.com>
2025-05-14 13:56:22 +0530

    Added Filereader and scanner

--- P_10_1.java
+++ P_10_1.java
@@ -3,7 +3,13 @@ import java.util.Scanner;
 
 public class P_10_1 {
     public static void main(String... args) throws IOException{
-       
+        int co = 0;
+        FileReader fr = new FileReader("p_10_1.txt");
+        Scanner fd = new Scanner(fr);
+
+        String w = "done";
+
+        System.out.println(w + " has occured " + co + " times.");
     }
 }
 
