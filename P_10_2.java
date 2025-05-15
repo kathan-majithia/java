0cade924066c7e0182764c73abcc458c94783427
unknown <kathanmajithia@gmail.com>
2025-05-15 13:42:52 +0530

    Added try block

--- P_10_2.java
+++ P_10_2.java
@@ -2,6 +2,10 @@ import java.io.*;
 
 public class P_10_2 {
     public static void main(String... args) {
-             
+        try(BufferedReader red = new BufferedReader(new FileReader("P_10_2.java"))){
+            
+        }catch(IOException e){
+            System.out.println(e);
+        }     
     }
 }
