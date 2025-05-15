65bf84571632e1157ab9e870fc995f083af1ce34
unknown <kathanmajithia@gmail.com>
2025-05-15 13:43:09 +0530

    Complete

--- P_10_2.java
+++ P_10_2.java
@@ -3,7 +3,9 @@ import java.io.*;
 public class P_10_2 {
     public static void main(String... args) {
         try(BufferedReader red = new BufferedReader(new FileReader("P_10_2.java"))){
-            
+            String line;
+            while((line = red.readLine()) != null)
+                System.out.println(line);
         }catch(IOException e){
             System.out.println(e);
         }     
