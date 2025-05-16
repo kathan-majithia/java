fa78928f445bdbfd2633eea40d7ba19e7c2c68fa
unknown <kathanmajithia@gmail.com>
2025-05-16 21:56:42 +0530

    Getting all files

--- P_10_3.java
+++ P_10_3.java
@@ -4,8 +4,10 @@ public class P_10_3 {
     public static void main(String... args) {
         File f = new File("F:\\SEM 4\\12302040501025_Java\\Collection");
         if(f.isDirectory()){
-           
-            }
+            System.out.println("Dir : " + f.getPath());
+            String li[] = f.list();
+            System.out.println("File names : ");
+        }
         }else{
             System.out.println("It is not a directory");
         }
