6a6b1b73663decd82369d28b41d6042699b41a15
unknown <kathanmajithia@gmail.com>
2025-05-16 21:56:18 +0530

    Checking dir

--- P_10_3.java
+++ P_10_3.java
@@ -2,6 +2,12 @@ import java.io.*;
 
 public class P_10_3 {
     public static void main(String... args) {
-        
+        File f = new File("F:\\SEM 4\\12302040501025_Java\\Collection");
+        if(f.isDirectory()){
+           
+            }
+        }else{
+            System.out.println("It is not a directory");
+        }
     }
 }
