2f354c294f9065283d3bb280de138b0df67f07f4
unknown <kathanmajithia@gmail.com>
2025-05-17 11:28:35 +0530

    Added client address

--- P_11_ser.java
+++ P_11_ser.java
@@ -6,7 +6,12 @@ public class P_11_ser extends Thread{
     final String ip = "192.168.21.61";
     
     void connect() throws Exception{
-        
+        ServerSocket ss = new ServerSocket(port);
+        Socket cs = ss.accept();
+        Thread.sleep(1000);
+
+        System.out.println("Client connected : " + cs.getInetAddress().getHostAddress());
+        Thread.sleep(1000);
     }
     public static void main(String... args) {
         P_11_ser ser = new P_11_ser();
