ca7e92362af20779c91c79cf1e8780b8b085048e
unknown <kathanmajithia@gmail.com>
2025-05-17 11:29:13 +0530

    Receiving message from client

--- P_11_ser.java
+++ P_11_ser.java
@@ -12,6 +12,20 @@ public class P_11_ser extends Thread{
 
         System.out.println("Client connected : " + cs.getInetAddress().getHostAddress());
         Thread.sleep(1000);
+
+        InputStream is = cs.getInputStream();
+        OutputStream os = cs.getOutputStream();
+
+        PrintWriter wr = new PrintWriter(os,true);
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(is));
+        String line;
+        while((line = br.readLine()) != null)
+            System.out.println("Message from client : " + line);
+        cs.close();
+        ss.close();
+        wr.close();
+
     }
     public static void main(String... args) {
         P_11_ser ser = new P_11_ser();
